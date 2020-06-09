b7053fa855f33a1a249f057fe6886f9377b9c4ca
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-09 15:32:16 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -4,10 +4,8 @@ max_value = -1
   while count < array.length do
     if max_value < array(count)
       max_value = array(count)
-
     end
       count += 1
-      binding.pry
   end
   max_value
 end
