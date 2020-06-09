f219524f1ff11f0282f683e6d375a6749da90818
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-09 15:31:37 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -10,12 +10,3 @@ max_value = -1
   max_value
 end
 
-# Pseduo code:
-
-# Check how long the array is
-#   while the count is less then total length of array run loop
-#     we will access each element by using the array(count)
-#     if the array count is greater then the maximum number
-#       then shovel the array(count) into maxium number variable
-#     end
-#     maxium_number
