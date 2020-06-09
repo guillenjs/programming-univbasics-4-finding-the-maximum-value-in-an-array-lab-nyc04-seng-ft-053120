c9e5996e6ddfb75c41825ac61f01e0028b00778a
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-09 15:25:01 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,11 @@
 def find_max_value(array)
   count = 0
-
+max_value = -1
   while count < array.length do
-
-      count += 1
+    if max_value < array(count)
+      max_value = array(count)
     end
+    max_value
 end
 
 # Pseduo code:
